ff4eb2895f3d01d3ac837022ca099aa77059b452
Sotheanith Sok <thetheanith@gmail.com>
2019-01-31 12:33:53 -0800

    Add lab 1 and lab 2

---
+++ Lab 2/hello.f
@@ -0,0 +1,9 @@
+	program first
+* For a little fun let's write spaghetti code	
+	  	goto 100
+10     	goto 200 
+20     	print*, 'Hello World from FORTRAN 77'
+	  	stop
+100    	goto 10
+200	  	goto 20
+       	end
